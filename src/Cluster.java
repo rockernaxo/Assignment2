01d10880d977d6203d0e7858556679e6814bf486
Ignacio <rockernaxo@gmail.com>
2017-06-08 21:43:06 +0200

    Changes

--- src/Cluster.java
+++ src/Cluster.java
@@ -7,12 +7,14 @@ public class Cluster {
 	private List<Point> points;
 	private Point centroid;
 	private int id;
+	private String label;
 
 	// Creates a new Cluster
 	public Cluster(int id, Point centroid) {
 		this.id = id;
 		this.points = new ArrayList<Point>();
 		this.centroid = centroid;
+		this.label="";
 	}
 
 	public Point getCentroid() {
@@ -33,5 +35,13 @@ public class Cluster {
 	
 	public void addPoint(Point point) {
 		this.points.add(point);
+	}
+	
+	public void setLabel(String label){
+		this.label=label;
+	}
+	
+	public String getLabel(){
+		return this.label;
 	}
 }
